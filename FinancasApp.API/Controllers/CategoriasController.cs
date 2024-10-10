fe34c0646b71b13e918575c0879129c0355427a8
CristovaoAlexander <alex.cafr.fac@gmail.com>
2024-10-10 19:43:33 -0300

    first commit

---
+++ FinancasApp.API/Controllers/CategoriasController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinancasApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ FinancasApp.API/Controllers/ContasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinancasApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContasController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{datamin}/{datamax}")]
+        public IActionResult Get(DateTime datamin, DateTime datamax )
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            return Ok();
+        }
+    }
+}
